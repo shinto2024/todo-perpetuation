f937659a2cc573265c88a67f55457a17dc8d971b
taigaozawa <taigaozawa@gmail.com>
2021-03-23 19:10:50 +0900

    Docker 対応

--- index.js
+++ index.js
@@ -1,47 +1,29 @@
 'use strict';
 // { name: タスクの文字列, state: 完了しているかどうかの真偽値 }
-const tasks = new Array();
+const tasks = [];
 
 /**
-* TODOを追加する
-* @param {string} task
-*/
+ * TODOを追加する
+ * @param {string} task
+ */
 function add(task) {
   tasks.push({ name: task, state: false });
 }
 
 /**
-* タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了したかを返す
-* @param {object} taskAndIsDonePair
-* @return {boolean} 完了したかどうか
-*/
-function isDone(taskAndIsDonePair) {
-  return taskAndIsDonePair.state;
-}
-
-/**
-* タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了していないかを返す
-* @param {object} taskAndIsDonePair
-* @return {boolean} 完了していないかどうか
-*/
-function isNotDone(taskAndIsDonePair) {
-  return !isDone(taskAndIsDonePair);
-}
-
-/**
-* TODO一覧の配列を取得する
+* TODOの一覧の配列を取得する
 * @return {array}
 */
 function list() {
   return tasks
-    .filter(isNotDone)
+    .filter(task => !task.state)
     .map(t => t.name);
 }
 
 /**
-* TODOを完了状態にする
-* @param {string} task
-*/
+ * TODOを完了状態にする
+ * @param {string} task
+ */
 function done(task) {
   const indexFound = tasks.findIndex(t => t.name === task);
   if (indexFound !== -1) {
@@ -50,19 +32,19 @@ function done(task) {
 }
 
 /**
-* 完了済みのタスクの一覧の配列を取得する
-* @return {array}
-*/
+ * 完了済みのタスクの一覧の配列を取得する
+ * @return {array}
+ */
 function donelist() {
   return tasks
-    .filter(isDone)
+    .filter(task => task.state)
     .map(t => t.name);
 }
 
 /**
-* 項目を削除する
-* @param {string} task
-*/
+ * 項目を削除する
+ * @param {string} task
+ */
 function del(task) {
   const indexFound = tasks.findIndex(t => t.name === task);
   if (indexFound !== -1) {
